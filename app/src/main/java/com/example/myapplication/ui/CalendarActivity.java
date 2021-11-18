6e3f1073f9f180e3aa6c05f9492ca8c0516d05aa
Crimsey <kubsok2@wp.pl>
2021-11-18 14:46:34 +0100

    wstepne dodawanie danych do Firestore'a dziala

--- app/src/main/java/com/example/myapplication/ui/CalendarActivity.java
+++ app/src/main/java/com/example/myapplication/ui/CalendarActivity.java
@@ -1,26 +1,34 @@
 package com.example.myapplication.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.myapplication.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.time.LocalDate;
 import java.time.Year;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class CalendarActivity extends NavigationActivity implements CalendarAdapter.OnItemListener {
+    private static final String TAG = "CalendarActivity";
     private TextView monthYearText;
     private RecyclerView calendarRecyclerView;
     private LocalDate selectedDate;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,7 @@ public class CalendarActivity extends NavigationActivity implements CalendarAdap
         initWidgets();
         selectedDate = LocalDate.now();
         setMonthView();
+        //setupFirebaseAuth();
     }
 
     private void setMonthView() {
@@ -87,4 +96,47 @@ public class CalendarActivity extends NavigationActivity implements CalendarAdap
             Toast.makeText(this, message, Toast.LENGTH_LONG).show();
         }
     }
+
+    /*
+    ----------------------------- Firebase setup ---------------------------------
+ */
+    /*private void setupFirebaseAuth(){
+        Log.d(TAG, "setupFirebaseAuth: started.");
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    //Toast.makeText(CalendarActivity.this, "Signed in", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(CalendarActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+
+
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+    }*/
+
+    /*@Override
+    public void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
+        }
+    }*/
 }
--- app/src/main/java/com/example/myapplication/ui/SomewhereActivity.java
+++ app/src/main/java/com/example/myapplication/ui/SomewhereActivity.java
@@ -2,14 +2,169 @@ package com.example.myapplication.ui;
 
 
 import android.os.Bundle;
+import android.view.View;
+
 import com.example.myapplication.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import com.example.myapplication.ui.models.Note;
+
+public class SomewhereActivity extends NavigationActivity implements View.OnClickListener,ISomewhereActivity {
+
+    private static final String TAG = "SomewhereActivity";
+
+    //Firebase
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
 
-public class SomewhereActivity extends NavigationActivity {
+    //widgets
+    private FloatingActionButton mFab;
+
+    //vars
+    View mParentLayout;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_somewhere);
+        mFab = findViewById(R.id.fab);
+        mParentLayout = findViewById(android.R.id.content);
+
+        //setupFirebaseAuth();
+        mFab.setOnClickListener(this);
     }
+
+    @Override
+    public void createNewNote(String title, String content) {
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        DocumentReference newNoteRef = db
+                .collection("notes")
+                .document();
+
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        Note note = new Note();
+        note.setTitle(title);
+        note.setContent(content);
+        note.setNote_id(newNoteRef.getId());
+        note.setUser_id(userId);
+
+        newNoteRef.set(note).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    makeSnackBarMessage("Created new note");
+                }
+                else{
+                    makeSnackBarMessage("Failed. Check log.");
+                }
+            }
+        });
+    }
+
+    private void makeSnackBarMessage(String message){
+        Snackbar.make(mParentLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+
+            case R.id.fab:{
+                //create a new note
+                NewNoteDialog dialog = new NewNoteDialog();
+                dialog.show(getSupportFragmentManager(), getString(R.string.dialog_new_note));
+                break;
+            }
+        }
+    }
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.optionSignOut:
+                signOut();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void signOut(){
+        Log.d(TAG, "signOut: signing out");
+        FirebaseAuth.getInstance().signOut();
+    }*/
+
+    /*
+            ----------------------------- Firebase setup ---------------------------------
+         */
+    private void setupFirebaseAuth(){
+        Log.d(TAG, "setupFirebaseAuth: started.");
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+
+                } else {
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    Intent intent = new Intent(SomewhereActivity.this, LoginActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+    }
+
+    /*@Override
+    public void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
+        }
+    }*/
 }
