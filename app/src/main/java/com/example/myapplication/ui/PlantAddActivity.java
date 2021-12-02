1c3e14b0e3a072a003f48d89c04fe4cfa7e52b5b
Crimsey <kubsok2@wp.pl>
2021-12-02 17:27:25 +0100

    zdjecie

--- app/src/main/java/com/example/myapplication/ui/PlantAddActivity.java
+++ app/src/main/java/com/example/myapplication/ui/PlantAddActivity.java
@@ -2,14 +2,20 @@ package com.example.myapplication.ui;
 
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.basgeekball.awesomevalidation.AwesomeValidation;
@@ -17,47 +23,33 @@ import com.basgeekball.awesomevalidation.ValidationStyle;
 import com.example.myapplication.R;
 import com.example.myapplication.ui.models.Plant;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.core.Context;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.UUID;
 
 import butterknife.ButterKnife;
 
 public class PlantAddActivity extends AppCompatActivity {
 
     private static final String TAG = "PlantAddActivity";
-
-    /*@BindView(R.id.plantName)
-    TextInputLayout plantName;
-    @BindView(R.id.plantType)
-    TextInputLayout plantType;
-    @BindView(R.id.plantNote)
-    TextInputLayout plantNote;
-    @SuppressLint("NonConstantResourceId")
-    @BindView(R.id.datePlanting2)
-    TextInputEditText datePlanting;
-    //@BindView(R.id.plantPicture)
-    //TextInputLayout plantPicture;
-    @BindView(R.id.plantInsolation)
-    TextInputLayout plantInsolation;
-    @BindView(R.id.soilHumidity)
-    TextInputLayout soilHumidity;
-    /*@BindView(R.id.airHumidity)
-    TextInputLayout airHumidity;
-    @BindView(R.id.plantNutrient)
-    TextInputLayout plantNutrient;
-    @BindView(R.id.plantIsPoison)
-    Spinner plantIsPoison;*/
 
     TextInputLayout plantName;
     Spinner plantType;
@@ -72,7 +64,10 @@ public class PlantAddActivity extends AppCompatActivity {
     AwesomeValidation validator;
 
     private IPlantAddActivity mIPlantAddActivity;
-
+    private ImageView profilePic;
+    public Uri imageUri;
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,6 +82,17 @@ public class PlantAddActivity extends AppCompatActivity {
         datePlanting = findViewById(R.id.datePlanting2);
         plantInsolation = findViewById(R.id.plantInsolation);
         soilHumidity = findViewById(R.id.soilHumidity);
+        profilePic = findViewById(R.id.profilePic);
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+        profilePic.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                choosePicture();            }
+        }
+        );
 
         setupRules();
 
@@ -106,6 +112,53 @@ public class PlantAddActivity extends AppCompatActivity {
                 addPlant();
             }
         });
+
+    }
+
+    private void choosePicture() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode,data);
+        if(requestCode==1 && resultCode==RESULT_OK && data!=null && data.getData()!=null){
+            imageUri = data.getData();
+            profilePic.setImageURI(imageUri);
+            uploadPicture();
+        }
+    }
+
+    private void uploadPicture() {
+        //final ProgressBar pd = new ProgressBar();
+        final String randomKey = UUID.randomUUID().toString();
+        StorageReference riversRef = storageReference.child("images/" + randomKey);
+
+        riversRef.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        //Snackbar.make(findViewById(R.layout.activity_plantadd), "Image Uploaded.", Snackbar.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(),"Image Uploaded.",Toast.LENGTH_LONG).show();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(),"Failed To Upload",Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                        double progressPercent = (100.00 *taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                        //pd.showContextMenu("Percetage: " + (int) progressPercent);
+                    }
+                });
 
     }
 
